d5dfe888a574ae4df1b4440e264e6b9e9f9168fa
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-09-23 12:48:48 -0600

    feat: Pruebas

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from app.models.company import Company
+from app.models.company_user import CompanyUser
+from app.models.user import User
 
-__all__ = ["Company"]
+__all__ = ["Company", "CompanyUser", "User"]
