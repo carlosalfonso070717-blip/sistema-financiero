1ba1c4a9ed0d7010bdf487c6ca3c558e32dc0491
carlosalfonso070717-blip <carlosalfonso070717@gmail.com>
2026-09-23 20:02:04 -0600

    Registra las nuevas entidades en el paquete de modelos

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,17 @@
+from app.models.account import Account, AccountType
+from app.models.account_concept import AccountConcept
 from app.models.company import Company
 from app.models.company_user import CompanyUser
+from app.models.concept import Concept, ConceptType
 from app.models.user import User
 
-__all__ = ["Company", "CompanyUser", "User"]
+__all__ = [
+    "Company",
+    "User",
+    "CompanyUser",
+    "Account",
+    "AccountType",
+    "Concept",
+    "ConceptType",
+    "AccountConcept",
+]
